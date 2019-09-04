50d62966ae9b01d06303dee73716b71fb87ef5f8
zhaotong <kris_zt@163.com>
2019-09-04 10:50:33 +0800

    fix

--- src/main/java/com/kris/docker_spring_boot/controller/TestController.java
+++ src/main/java/com/kris/docker_spring_boot/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @RequestMapping("/index")
+    @RequestMapping("/test")
     public String show() {
         return "===================>>>Hello World<<<===============";
     }
