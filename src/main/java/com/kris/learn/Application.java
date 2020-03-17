172c00d5476d359e2aeb905e8f36da1881f518db
zhaotong <447254141@qq.com>
2020-03-17 22:03:47 +0800

    fix

--- src/main/java/com/kris/learn/Application.java
+++ src/main/java/com/kris/learn/Application.java
@@ -1,4 +1,4 @@
-package com.kris.docker_spring_boot;
+package com.kris.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
